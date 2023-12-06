f50b2bc5127e3894573b9929b231eed349ef6347
Andreas Berg <andreas@edupihi.eu>
2023-12-06 11:57:27 +0200

    dev

--- components/ep_em_bl0939/sensor.py
+++ components/ep_em_bl0939/sensor.py
@@ -4,28 +4,20 @@ from esphome.components import sensor, uart
 from esphome.const import (
     CONF_ID,
     CONF_VOLTAGE,
-    CONF_FREQUENCY,
     DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_ENERGY,
     DEVICE_CLASS_POWER,
     DEVICE_CLASS_VOLTAGE,
-    DEVICE_CLASS_FREQUENCY,
-    DEVICE_CLASS_ENERGY,
     STATE_CLASS_MEASUREMENT,
-    UNIT_EMPTY,
-    UNIT_VOLT,
+    STATE_CLASS_TOTAL_INCREASING,
     UNIT_AMPERE,
-    UNIT_WATT,
-    UNIT_HERTZ,
     UNIT_KILOWATT_HOURS,
+    UNIT_VOLT,
+    UNIT_WATT,
 )
 
 CODEOWNERS = ["@pihiandreas"]
 DEPENDENCIES = ["uart"]
-
-cse7761_ns = cg.esphome_ns.namespace("cse7761")
-CSE7761Component = cse7761_ns.class_(
-    "CSE7761Component", cg.PollingComponent, uart.UARTDevice
-)
 
 CONF_CURRENT_1 = "current_1"
 CONF_CURRENT_2 = "current_2"
@@ -33,24 +25,19 @@ CONF_ACTIVE_POWER_1 = "active_power_1"
 CONF_ACTIVE_POWER_2 = "active_power_2"
 CONF_ENERGY_1 = "energy_1"
 CONF_ENERGY_2 = "energy_2"
-CONF_ENERGY_COUNTER_1 = "energy_counter_1"
-CONF_ENERGY_COUNTER_2 = "energy_counter_2"
+CONF_ENERGY_TOTAL = "energy_total"
 
+bl0939_ns = cg.esphome_ns.namespace("bl0939")
+BL0939 = bl0939_ns.class_("BL0939", cg.PollingComponent, uart.UARTDevice)
 
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(CSE7761Component),
+            cv.GenerateID(): cv.declare_id(BL0939),
             cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(
                 unit_of_measurement=UNIT_VOLT,
                 accuracy_decimals=1,
                 device_class=DEVICE_CLASS_VOLTAGE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_FREQUENCY): sensor.sensor_schema(
-                unit_of_measurement=UNIT_HERTZ,
-                accuracy_decimals=2,
-                device_class=DEVICE_CLASS_FREQUENCY,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_CURRENT_1): sensor.sensor_schema(
@@ -67,13 +54,13 @@ CONFIG_SCHEMA = (
             ),
             cv.Optional(CONF_ACTIVE_POWER_1): sensor.sensor_schema(
                 unit_of_measurement=UNIT_WATT,
-                accuracy_decimals=1,
+                accuracy_decimals=0,
                 device_class=DEVICE_CLASS_POWER,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_ACTIVE_POWER_2): sensor.sensor_schema(
                 unit_of_measurement=UNIT_WATT,
-                accuracy_decimals=1,
+                accuracy_decimals=0,
                 device_class=DEVICE_CLASS_POWER,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
@@ -81,34 +68,24 @@ CONFIG_SCHEMA = (
                 unit_of_measurement=UNIT_KILOWATT_HOURS,
                 accuracy_decimals=3,
                 device_class=DEVICE_CLASS_ENERGY,
-                state_class=STATE_CLASS_MEASUREMENT,
+                state_class=STATE_CLASS_TOTAL_INCREASING,
             ),
             cv.Optional(CONF_ENERGY_2): sensor.sensor_schema(
                 unit_of_measurement=UNIT_KILOWATT_HOURS,
                 accuracy_decimals=3,
                 device_class=DEVICE_CLASS_ENERGY,
-                state_class=STATE_CLASS_MEASUREMENT,
+                state_class=STATE_CLASS_TOTAL_INCREASING,
             ),
-            cv.Optional(CONF_ENERGY_COUNTER_1): sensor.sensor_schema(
-                unit_of_measurement=UNIT_EMPTY,
-                accuracy_decimals=0,
+            cv.Optional(CONF_ENERGY_TOTAL): sensor.sensor_schema(
+                unit_of_measurement=UNIT_KILOWATT_HOURS,
+                accuracy_decimals=3,
                 device_class=DEVICE_CLASS_ENERGY,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_ENERGY_COUNTER_2): sensor.sensor_schema(
-                unit_of_measurement=UNIT_EMPTY,
-                accuracy_decimals=0,
-                device_class=DEVICE_CLASS_ENERGY,
-                state_class=STATE_CLASS_MEASUREMENT,
+                state_class=STATE_CLASS_TOTAL_INCREASING,
             ),
         }
     )
     .extend(cv.polling_component_schema("60s"))
     .extend(uart.UART_DEVICE_SCHEMA)
-)
-
-FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
-    "cse7761", baud_rate=38400, require_rx=True, require_tx=True
 )
 
 
@@ -117,20 +94,27 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    for key in [
-        CONF_VOLTAGE,
-        CONF_FREQUENCY,
-        CONF_CURRENT_1,
-        CONF_CURRENT_2,
-        CONF_ACTIVE_POWER_1,
-        CONF_ACTIVE_POWER_2,
-        CONF_ENERGY_1,
-        CONF_ENERGY_2,
-        CONF_ENERGY_COUNTER_1,
-        CONF_ENERGY_COUNTER_2,
-    ]:
-        if key not in config:
-            continue
-        conf = config[key]
-        sens = await sensor.new_sensor(conf)
-        cg.add(getattr(var, f"set_{key}_sensor")(sens))
+    if voltage_config := config.get(CONF_VOLTAGE):
+        sens = await sensor.new_sensor(voltage_config)
+        cg.add(var.set_voltage_sensor(sens))
+    if current_1_config := config.get(CONF_CURRENT_1):
+        sens = await sensor.new_sensor(current_1_config)
+        cg.add(var.set_current_sensor_1(sens))
+    if current_2_config := config.get(CONF_CURRENT_2):
+        sens = await sensor.new_sensor(current_2_config)
+        cg.add(var.set_current_sensor_2(sens))
+    if active_power_1_config := config.get(CONF_ACTIVE_POWER_1):
+        sens = await sensor.new_sensor(active_power_1_config)
+        cg.add(var.set_power_sensor_1(sens))
+    if active_power_2_config := config.get(CONF_ACTIVE_POWER_2):
+        sens = await sensor.new_sensor(active_power_2_config)
+        cg.add(var.set_power_sensor_2(sens))
+    if energy_1_config := config.get(CONF_ENERGY_1):
+        sens = await sensor.new_sensor(energy_1_config)
+        cg.add(var.set_energy_sensor_1(sens))
+    if energy_2_config := config.get(CONF_ENERGY_2):
+        sens = await sensor.new_sensor(energy_2_config)
+        cg.add(var.set_energy_sensor_2(sens))
+    if energy_total_config := config.get(CONF_ENERGY_TOTAL):
+        sens = await sensor.new_sensor(energy_total_config)
+        cg.add(var.set_energy_sensor_sum(sens))
